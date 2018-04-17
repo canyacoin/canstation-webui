53f1bdd81bf4519cf41532ddc19fdc8a6437db92
Ahmad ElSokkary <ahmed.sokkary@gmail.com>
2018-04-17 11:10:29 +1000

    change .firebaserc to another project and fix api urls to the new firebase proj domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,9 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyCAL5Z5g1F4RIowZh0vp_9D3QsfHN6LqEc',
-    authDomain: 'caninvoice-9286f.firebaseapp.com',
-    databaseURL: 'https://caninvoice-9286f.firebaseio.com',
-    projectId: 'caninvoice-9286f',
-    storageBucket: 'caninvoice-9286f.appspot.com',
-    messagingSenderId: '313965483789'
-  },
   gasStation: {
     waitToRefetchInSec: 60,
     url: {
-      priceEstimate: 'https://us-central1-canyagasstation-a98a8.cloudfunctions.net/api/gas-estimate/average',
+      priceEstimate: 'https://us-central1-canstation-46066.cloudfunctions.net/api/gas-estimate/average',
       predictTable: 'https://ethgasstation.info/json/predictTable.json'
     }
   },
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,18 +5,10 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyCAL5Z5g1F4RIowZh0vp_9D3QsfHN6LqEc',
-    authDomain: 'caninvoice-9286f.firebaseapp.com',
-    databaseURL: 'https://caninvoice-9286f.firebaseio.com',
-    projectId: 'caninvoice-9286f',
-    storageBucket: 'caninvoice-9286f.appspot.com',
-    messagingSenderId: '313965483789'
-  },
   gasStation: {
     waitToRefetchInSec: 60,
     url: {
-      priceEstimate: 'http://localhost:5000/canyagasstation-a98a8/us-central1/api/gas-estimate/average', //'https://ethgasstation.info/json/ethgasAPI.json',
+      priceEstimate: 'http://localhost:5000/canstation-46066/us-central1/api/gas-estimate/average', // 'https://ethgasstation.info/json/ethgasAPI.json',
       predictTable: 'https://ethgasstation.info/json/predictTable.json'
     }
   },
